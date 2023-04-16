a997d210637bb2743620bed9c292f2f48d06d0f7
Marcello Novak <marcellonovak@live.com>
2023-04-15 21:17:13 -0700

    Edited the tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,19 +3,19 @@ Test the main module.
 Author: Wolf Paulus (wolf@paulus.com)
 """
 from unittest import TestCase
-from main import is_odd, is_odd_str
+from __main__ import is_prime, is_prime_str
 
 
 class Test(TestCase):
-    def test_is_odd(self):
-        assert not is_odd(0)
-        assert is_odd(1)
-        assert not is_odd(2)
+    def test_is_prime(self):
+        assert not is_prime(0)
+        assert is_prime(7)
+        assert not is_prime(16)
 
-    def test_is_odd_str(self):
-        assert is_odd_str("0") == "0 is even."
-        assert is_odd_str("1") == "1 is odd."
-        assert is_odd_str("2") == "2 is even."
-        assert is_odd_str("-1") == "Please enter a number."
-        assert is_odd_str("A") == "Please enter a number."
-        assert is_odd_str("") == "Please enter a number."
+    def test_is_prime_str(self):
+        assert is_prime_str("11") == "11 is prime."
+        assert is_prime_str("13") == "13 is prime."
+        assert is_prime_str("20") == "20 is not prime."
+        assert is_prime_str("-1") == "Please enter a number."
+        assert is_prime_str("A") == "Please enter a number."
+        assert is_prime_str("") == "Please enter a number."
